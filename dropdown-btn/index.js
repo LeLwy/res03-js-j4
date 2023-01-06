5bf42a9d51159566d703579bbd58735d61bf1b88
LeLwy <louis.chancioux@gmail.com>
2023-01-06 14:26:51 +0000

    Dropdown Button

---
+++ dropdown-btn/index.js
@@ -0,0 +1,19 @@
+let menu = ["home", "blog", "contact"]
+
+function dropMenu(){
+    
+    let btn = document.getElementById("d-down");
+    let count = 0
+    
+    btn.addEventListener("click", function(){
+       
+       for(item of menu){
+           
+           let hiddenList = document.getElementById("hidden-list")
+           let li = document.createElement("li");
+           
+           
+       } 
+        
+    });
+}
